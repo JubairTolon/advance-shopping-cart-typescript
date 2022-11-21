6f5d20abf3d2f7cd0930c8e7a6108d903444430c
Jubair Ahammed Tolon <zs.tolon0073@gamil.com>
2022-11-21 20:25:12 +0600

    project init & navbar create

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Container>
+    </>
+  )
+
+}
+
+export default App
---
+++ src/pages/Store.tsx
@@ -0,0 +1,3 @@
+export const Store = () => {
+    return <h1>This is Store</h1>
+}
