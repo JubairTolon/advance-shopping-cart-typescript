1fb74e2f6d228c7cd63b0861a53d5e6742fbd879
Jubair Ahammed Tolon <zs.tolon0073@gamil.com>
2022-11-21 23:56:31 +0600

    nav route & store data load

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,3 +1,3 @@
 export const Contact = () => {
-    return <h1>This is cotact</h1>
+    return <h1>This is contact</h1>
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,3 @@
 export const Home = () => {
-    return <h1>This is home</h1>
+    return <h1>Home</h1>
 }
